29b85947358e36948b94166c4d7defa0bb7dc25d
MATEUS MULLER BISCARO <mateus_biscaro@estudante.sesisenai.org.br>
2025-04-02 10:48:07 -0300

    add gitignore

--- texte.js
+++ texte.js
@@ -1,3 +1,7 @@
-function soma(a,b){
+function soma(a, b){
     return a + b
 } module.exports = {soma}
+
+function subtracao(a, b){
+    return a - b
+} module.exports = {subtracao}
--- texte2.js
+++ texte2.js
@@ -1,20 +1,29 @@
 const http = require('http')
 const { soma } = require('./texte')
+const { subtracao } = require('./texte')
 const url = require('url')
 const PORT = 8000
 
 //cria o server
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
     const urlcapturada = url.parse(req.url, true)
-    const {query} = urlcapturada
-    let resultado = soma(Number(query.a), Number(query.b))
+    const { query, pathname } = urlcapturada
+    let resultado = 0
+    if(pathname === './soma'){
+        resultado = soma(Number(query.a), Number(query.b))
+    }else if(pathname === './subtracao'){
+        resultado = subtracao(Number(query.a), Number(query.b))
+    }else{
+        res.statusCode(404).end('pagina nao encontada')
+    }  
+
     console.log(urlcapturada)
-    res.end(`aqui vai um resultado ${resultado}`) //soma serve para nada, so para texte mesmo
+    res.end(`aqui vai um resultado ${resultado}`)
 })
 // fazer ele ouvir a porta 8000
 
-server .listen(PORT, ()=>{
+server.listen(PORT, () => {
     console.log(`server iniciando na porta ${PORT}`)
-    
+
 })
